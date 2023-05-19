36a2352790d19d193ffa7782eb9f5388dc28a76d
Патрикеев Д.Н <patrikeevdn@list.ry>
2023-05-19 16:33:48 +0300

    add async

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import asyncio
+import configparser
 import json
 import re
-import configparser
 
-import telebot
-from telebot import types
+from telebot import types, asyncio_filters
+from telebot.async_telebot import AsyncTeleBot
+from telebot.asyncio_storage import StateMemoryStorage
 from telebot.types import Message
+from telebot.asyncio_handler_backends import State, StatesGroup
 
 # class MyBot:
 #
@@ -16,9 +19,21 @@ config.read("setting.ini")
 token = config["Bot"]["bot"]
 payment_token = config["Bot"]["payment"]
 PRICE = types.LabeledPrice(label="Звезда смерти", amount=int(config["Price"]["zvezda"])*100)  # в копейках (руб)
-bot = telebot.TeleBot(token)
+bot = AsyncTeleBot(token, state_storage=StateMemoryStorage())
 
-def validate_number(number):
+
+class MyStates(StatesGroup):
+    start = State() # statesgroup should contain states
+    number = State()
+    bird = State()
+    buy = State()
+
+
+# class Bot:
+#     def __init__(self) -> None:
+#         super().__init__()
+
+async def validate_number(number):
     pattern = "\D"
     matter = re.findall(pattern, number)
     if not matter:
@@ -26,16 +41,17 @@ def validate_number(number):
     else:
         return False
 
-def check_correct_number(answ, number):
+async def check_correct_number(answ, number):
     if number == answ:
         return True
     else:
         return False
 
-def handler_number(message: Message):
+@bot.message_handler(state=MyStates.number)
+async def handler_number(message: Message):
     chat_id = message.chat.id
     request = message.text
-    if validate_number(request):
+    if await validate_number(request):
         with open("clients.json", "r") as ff:
             data_from_json = json.load(ff)
         data_from_json[chat_id] = {"number": request}
@@ -45,18 +61,20 @@ def handler_number(message: Message):
     else:
         mes = "Ответ написан с ошибкой. Напишите ответ без пробелов и сторонних символов. Сейчас он выглядит так: " + request
     correct_num = 13
-    if check_correct_number(correct_num, int(request)):
+    if await check_correct_number(correct_num, int(request)):
         mes = mes + " Вы посчитали правильно!"
     else:
         mes = mes + " К сожалению, вы ошиблись. Правильный ответ был {0}. У гусеницы {0} ножек.".format(correct_num)
-    bot.send_message(chat_id, mes)
-    bot.send_message(chat_id, "Вопрос 2. Сколько птиц пролетело за окном. Введите ваш ответ: ")
-    bot.register_next_step_handler(message, callback=handler_bird)
+    await bot.set_state(message.from_user.id, MyStates.bird, chat_id)
+    await bot.send_message(chat_id, mes)
+    await bot.send_message(chat_id, "Вопрос 2. Сколько птиц пролетело за окном. Введите ваш ответ: ")
+    # await bot.register_next_step_handler(message, callback=self.handler_bird)
 
-def handler_bird(message: Message):
+@bot.message_handler(state=MyStates.bird)
+async def handler_bird(message: Message):
     chat_id = message.chat.id
     request = message.text
-    if validate_number(request):
+    if await validate_number(request):
         with open("clients.json", "r") as ff:
             data_from_json = json.load(ff)
         data_from_json[chat_id] = {"bird": request}
@@ -66,25 +84,27 @@ def handler_bird(message: Message):
     else:
         mes = "Ответ написан с ошибкой. Напишите ответ без пробелов и сторонних символов. Сейчас он выглядит так: " + request
     correct_bird = 6
-    if check_correct_number(correct_bird, int(request)):
+    if await check_correct_number(correct_bird, int(request)):
         mes = mes + " Вы посчитали правильно!"
     else:
         mes = mes + " К сожалению, вы ошиблись. Правильный ответ был {0}. За окном было {0} птиц.".format(correct_bird)
-    bot.send_message(chat_id, mes)
+    await bot.send_message(chat_id, mes)
 
 @bot.message_handler(commands=["give_number"])
-def send_number(message: Message):
+async def send_number(message: Message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, "Вопрос 1. Посчитайте, пожалуйста, сколько ножек у гусениц. Введите свой ответ: ")
-    bot.register_next_step_handler(message, callback=handler_number)
+    await bot.set_state(message.from_user.id, MyStates.number, chat_id)
+    await bot.send_message(chat_id, "Вопрос 1. Посчитайте, пожалуйста, сколько ножек у гусениц. Введите свой ответ: ")
+
+    # await bot.register_next_step_handler(message, callback=self.handler_number)
 
 
 @bot.message_handler(commands=["buy"])
-def buy(message: Message):
+async def buy(message: Message):
     if payment_token.split(':')[1] == 'TEST':
-        bot.send_message(message.chat.id, "Тестовый платеж!!!")
+        await bot.send_message(message.chat.id, "Тестовый платеж!!!")
 
-    bot.send_invoice(message.chat.id,
+    await bot.send_invoice(message.chat.id,
                            title="Покупка Звезды Смерти",
                            description="Абсолютно новая Здезда Смерти",
                            provider_token=payment_token,
@@ -99,15 +119,23 @@ def buy(message: Message):
                            invoice_payload="test-invoice-payload")
 
 @bot.message_handler(content_types=["text"])
-def start(message: Message):
+async def start(message: Message):
     chat_id = message.chat.id
     request = message.text
+    user_id = message.from_user.id
     if request == '/start':
         response = "Здравствуйте. Этот бот может провести небольшой опрос. Для того, чтобы начать введите '/give_number'"
     elif request == '/help':
         response = "Это тестовый бот с ограниченным функционалом"
+    # elif request == '/buy':
+    #     await bot.set_state(user_id, MyStates.buy, chat_id)
+    #     response = "Вы перенаправляетесь в 'покупки'. Доступные лоты: {0}".format(PRICE.label)
     else:
         response = "Извините, бот вас не понял"
-    bot.send_message(chat_id, response)
+    await bot.set_state(user_id, MyStates.start, chat_id)
+    await bot.send_message(chat_id, response)
 
-bot.polling(non_stop=True, interval=0)
+bot.add_custom_filter(asyncio_filters.StateFilter(bot))
+bot.add_custom_filter(asyncio_filters.IsDigitFilter())
+
+asyncio.run(bot.polling(non_stop=True, interval=0))
