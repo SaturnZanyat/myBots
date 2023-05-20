3d7ed1a1bd3308c3bda262e8507a02f8ea50db10
Denis Patrikeev <patrikeevdn@list.ru>
2023-05-20 15:38:27 +0300

    init django rest framework

--- django/postgresTest/postgresTest/settings.py
+++ django/postgresTest/postgresTest/settings.py
@@ -10,19 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import configparser
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+config_file = os.path.join("..", "..", "setting.ini")
 config = configparser.ConfigParser()
-config.read("setting.ini")
+config.read(config_file)
+secrer = config["Django"]["secret"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config["Django"]["secret"]
+SECRET_KEY = secrer
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'testdb',
 ]
 
 MIDDLEWARE = [
